9681eb8e039122e386cf78a0f6d122adba2e96fb
Elite <elite.sytian@gmail.com>
2019-10-02 08:56:46 +0800

    added customizer to functions.php

--- functions.php
+++ functions.php
@@ -5,6 +5,9 @@ locate_template( "functions/setup.php", TRUE, TRUE );
 // PLUGIN FUNCTIONS & HOOKS
 locate_template( "functions/plugins.php", TRUE, TRUE );
 
+// FUNCTION CUSTOMIZER
+locate_template( "functions/customizer.php", TRUE, TRUE );
+
 // FUNCTION OVERWRITES 
 locate_template( "functions/overwrites.php", TRUE, TRUE );
 
